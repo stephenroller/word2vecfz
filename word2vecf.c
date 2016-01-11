1941c56a89490285a91157b4083b582c762834fd
Stephen Roller <roller@cs.utexas.edu>
2016-01-11 10:51:19 -0600

    Drop -iter argument.

--- word2vecf.c
+++ word2vecf.c
@@ -50,7 +50,6 @@ long long train_words = 0, word_count_actual = 0, file_size = 0;
 real alpha = 0.025, starting_alpha, sample = 0;
 real *syn0, *syn1, *syn1neg, *expTable;
 clock_t start;
-int numiters = 1;
 
 struct vocabulary *wv;
 struct vocabulary *cv;
@@ -127,73 +126,70 @@ void *TrainModelThread(void *id) {
   FILE *fi = fopen(train_file, "rb");
   long long start_offset = file_size / (long long)num_threads * (long long)id;
   long long end_offset = file_size / (long long)num_threads * (long long)(id+1);
-  int iter;
   //printf("thread %d %lld %lld \n",id, start_offset, end_offset);
-  for (iter=0; iter < numiters; ++iter) {
-     fseek(fi, start_offset, SEEK_SET);
-     // if not binary:
-     while (fgetc(fi) != '\n') { }; //TODO make sure its ok
+  fseek(fi, start_offset, SEEK_SET);
+  // if not binary:
+  while (fgetc(fi) != '\n') { }; //TODO make sure its ok
 
-     long long train_words = wv->word_count;
-     while (1) { //HERE @@@
-        // TODO set alpha scheduling based on number of examples read.
-        // The conceptual change is the move from word_count to pair_count
-        if (word_count - last_word_count > 10000) {
-           word_count_actual += word_count - last_word_count;
-           last_word_count = word_count;
-           if ((debug_mode > 1)) {
-              now=clock();
-              printf("%cAlpha: %f  Progress: %.2f%%  Words/thread/sec: %.2fk  ", 13, alpha,
-                    word_count_actual / (real)(numiters*train_words + 1) * 100,
-                    word_count_actual / ((real)(now - start + 1) / (real)CLOCKS_PER_SEC * 1000));
-              fflush(stdout);
-           }
-           alpha = starting_alpha * (1 - word_count_actual / (real)(numiters*train_words + 1));
-           if (alpha < starting_alpha * 0.0001) alpha = starting_alpha * 0.0001;
+  long long train_words = wv->word_count;
+  while (1) { //HERE @@@
+    // TODO set alpha scheduling based on number of examples read.
+    // The conceptual change is the move from word_count to pair_count
+    if (word_count - last_word_count > 10000) {
+        word_count_actual += word_count - last_word_count;
+        last_word_count = word_count;
+        if ((debug_mode > 1)) {
+          now=clock();
+          printf("%cAlpha: %f  Progress: %.2f%%  Words/thread/sec: %.2fk  ", 13, alpha,
+                word_count_actual / (real)(train_words + 1) * 100,
+                word_count_actual / ((real)(now - start + 1) / (real)CLOCKS_PER_SEC * 1000));
+          fflush(stdout);
         }
-        if (feof(fi) || ftell(fi) > end_offset) break;
-        for (c = 0; c < layer1_size; c++) neu1[c] = 0;
-        for (c = 0; c < layer1_size; c++) neu1e[c] = 0;
-        wrdi = ReadWordIndex(wv, fi);
-        ctxi = ReadWordIndex(cv, fi);
-        word_count++; //TODO ?
-        if (wrdi < 0 || ctxi < 0) continue;
+        alpha = starting_alpha * (1 - word_count_actual / (real)(train_words + 1));
+        if (alpha < starting_alpha * 0.0001) alpha = starting_alpha * 0.0001;
+    }
+    if (feof(fi) || ftell(fi) > end_offset) break;
+    for (c = 0; c < layer1_size; c++) neu1[c] = 0;
+    for (c = 0; c < layer1_size; c++) neu1e[c] = 0;
+    wrdi = ReadWordIndex(wv, fi);
+    ctxi = ReadWordIndex(cv, fi);
+    word_count++; //TODO ?
+    if (wrdi < 0 || ctxi < 0) continue;
 
-        if (sample > 0) {
-           real ran = (sqrt(wv->vocab[wrdi].cn / (sample * wv->word_count)) + 1) * (sample * wv->word_count) / wv->vocab[wrdi].cn;
-           next_random = next_random * (unsigned long long)25214903917 + 11;
-           if (ran < (next_random & 0xFFFF) / (real)65536) continue;
-           ran = (sqrt(cv->vocab[ctxi].cn / (sample * cv->word_count)) + 1) * (sample * cv->word_count) / cv->vocab[ctxi].cn;
-           next_random = next_random * (unsigned long long)25214903917 + 11;
-           if (ran < (next_random & 0xFFFF) / (real)65536) continue;
+    if (sample > 0) {
+        real ran = (sqrt(wv->vocab[wrdi].cn / (sample * wv->word_count)) + 1) * (sample * wv->word_count) / wv->vocab[wrdi].cn;
+        next_random = next_random * (unsigned long long)25214903917 + 11;
+        if (ran < (next_random & 0xFFFF) / (real)65536) continue;
+        ran = (sqrt(cv->vocab[ctxi].cn / (sample * cv->word_count)) + 1) * (sample * cv->word_count) / cv->vocab[ctxi].cn;
+        next_random = next_random * (unsigned long long)25214903917 + 11;
+        if (ran < (next_random & 0xFFFF) / (real)65536) continue;
+    }
+    //fread(&wrdi, 4, 1, fi);
+    //fread(&ctxi, 4, 1, fi);
+    // NEGATIVE SAMPLING
+    l1 = wrdi * layer1_size;
+    for (d = 0; d < negative + 1; d++) {
+        if (d == 0) {
+          target = ctxi;
+          label = 1;
+        } else {
+          next_random = next_random * (unsigned long long)25214903917 + 11;
+          target = unitable[(next_random >> 16) % table_size];
+          if (target == 0) target = next_random % (cv->vocab_size - 1) + 1;
+          if (target == ctxi) continue;
+          label = 0;
         }
-        //fread(&wrdi, 4, 1, fi);
-        //fread(&ctxi, 4, 1, fi);
-        // NEGATIVE SAMPLING
-        l1 = wrdi * layer1_size;
-        for (d = 0; d < negative + 1; d++) {
-           if (d == 0) {
-              target = ctxi;
-              label = 1;
-           } else {
-              next_random = next_random * (unsigned long long)25214903917 + 11;
-              target = unitable[(next_random >> 16) % table_size];
-              if (target == 0) target = next_random % (cv->vocab_size - 1) + 1;
-              if (target == ctxi) continue;
-              label = 0;
-           }
-           l2 = target * layer1_size;
-           f = 0;
-           for (c = 0; c < layer1_size; c++) f += syn0[c + l1] * syn1neg[c + l2];
-           if (f > MAX_EXP) g = (label - 1) * alpha;
-           else if (f < -MAX_EXP) g = (label - 0) * alpha;
-           else g = (label - expTable[(int)((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))]) * alpha;
-           for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1neg[c + l2];
-           for (c = 0; c < layer1_size; c++) syn1neg[c + l2] += g * syn0[c + l1];
-        }
-        // Learn weights input -> hidden
-        for (c = 0; c < layer1_size; c++) syn0[c + l1] += neu1e[c];
-     }
+        l2 = target * layer1_size;
+        f = 0;
+        for (c = 0; c < layer1_size; c++) f += syn0[c + l1] * syn1neg[c + l2];
+        if (f > MAX_EXP) g = (label - 1) * alpha;
+        else if (f < -MAX_EXP) g = (label - 0) * alpha;
+        else g = (label - expTable[(int)((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))]) * alpha;
+        for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1neg[c + l2];
+        for (c = 0; c < layer1_size; c++) syn1neg[c + l2] += g * syn0[c + l1];
+    }
+    // Learn weights input -> hidden
+    for (c = 0; c < layer1_size; c++) syn0[c + l1] += neu1e[c];
   }
   fclose(fi);
   free(neu1);
@@ -271,8 +267,6 @@ int main(int argc, char **argv) {
     printf(" in the training data will be randomly down-sampled; default is 0 (off), useful value in the original word2vec was 1e-5\n");
     printf("\t-alpha <float>\n");
     printf("\t\tSet the starting learning rate; default is 0.025\n");
-    printf("\t-iters <int>\n");
-    printf("\t\tPerform i iterations over the data; default is 1\n");
     printf("\t-binary <int>\n");
     printf("\t\tSave the resulting vectors in binary moded; default is 0 (off)\n");
     printf("\t-dumpcv filename\n");
@@ -302,7 +296,6 @@ int main(int argc, char **argv) {
   if ((i = ArgPos((char *)"-min-count", argc, argv)) > 0) min_count = atoi(argv[i + 1]);
   if ((i = ArgPos((char *)"-sample", argc, argv)) > 0) sample = atof(argv[i + 1]);
   if ((i = ArgPos((char *)"-dumpcv", argc, argv)) > 0) strcpy(dumpcv_file, argv[i + 1]);
-  if ((i = ArgPos((char *)"-iters", argc, argv)) > 0) numiters = atoi(argv[i+1]);
 
   if (output_file[0] == 0) { printf("must supply -output.\n\n"); return 0; }
   if (wvocab_file[0] == 0) { printf("must supply -wvocab.\n\n"); return 0; }
